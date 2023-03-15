1c5d87509869657b31bbea42a833b0fb9211a023
Justin Smethers <48101971+JustinSmethers@users.noreply.github.com>
2023-03-15 19:48:21 -0400

    added tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge: ['./backend/templates/**/*.html'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            // Add your custom theme configurations here
+        },
+    },
+    variants: {
+        extend: {
+            // Add your custom variants here
+        },
+    },
+    plugins: [],
+}
